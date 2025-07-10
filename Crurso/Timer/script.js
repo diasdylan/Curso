cdbd6db39a61f4a191dbedea65376e5d452a2711
Dylan Dias <dylandias@live.com>
2025-07-10 17:36:17 -0300

    The countdown timer was success!

--- Crurso/Timer/script.js
+++ Crurso/Timer/script.js
@@ -19,11 +19,11 @@ function timer(ev) {
         case '3':
             // alert('3')
             let timer3 = 30
-            let timer = setInterval(() => {
+            let time3 = setInterval(() => {
                 if (timer3 <= 0) {
-                    clearInterval(timer)
+                    clearInterval(time3)
                     showTime.innerText = 0;
-                    let salame = setInterval(()=>{clearInterval(salame); alert('done');},500);
+                    let salame3 = setInterval(() => { clearInterval(salame3); alert('done'); }, 500);
                 } else {
                     showTime.innerText = timer3;
                     timer3--;
@@ -32,20 +32,62 @@ function timer(ev) {
             break;
         case '6':
             let timer6 = 60
-            setInterval(() => {
-                if (timer6 == 0) {
-                    showTime.inert = 0;
-                    return alert('Done!')
+            let time6 = setInterval(() => {
+                if (timer6 <= 0) {
+                    clearInterval(time6)
+                    showTime.innerText = 0;
+                    let salame6 = setInterval(() => { clearInterval(salame6); alert('done'); }, 500);
                 } else {
-                    showTime.innerText = timer6--;
+                    showTime.innerText = timer6;
+                    timer6--;
                 }
             }, 1000)
             break;
         case '9':
-            alert('9')
+            let timer9 = 90;
+            let time9 = setInterval(() => {
+                if (timer9 <= 0) {
+                    clearInterval(time9);
+                    showTime.innerText = 0;
+                    let salame9 = setInterval(() => { clearInterval(salame9); alert('done'), 500 })
+                }
+                else {
+                    showTime.innerText = timer9
+                    timer9--;
+                }
+            }, 1000)
             break;
         case '12':
-            alert('12')
+            let timer12 = 120;
+            let time12 = setInterval(() => {
+                if (timer12 <= 0) {
+                    clearInterval(time12);
+                    showTime.innerText = 0;
+                    let salame12 = setInterval(() => { clearInterval(salame12); alert('done'), 500 })
+                }
+                else {
+                    showTime.innerText = timer12
+                    timer12--;
+                }
+            }, 1000)
+            break;
+        case 'c':
+            let choice = parseInt(prompt('Input the time for the timer'));
+            if (isNaN(choice)) {
+                return alert('Please insert a number');
+            }
+
+            let timerManual = setInterval(() => {
+                if (choice <= 0) {
+                    clearInterval(timerManual);
+                    showTime.innerText = choice;
+                    let salameManual = setInterval(() => { clearInterval(salameManual); alert('Done'), 500 })
+                }
+                else {
+                    showTime.innerText = choice;
+                    choice--
+                }
+            }, 1000)
             break;
         default:
             alert('select a value')

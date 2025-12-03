9c401f4391da173cdbc00bb2682b00fbf06a1793
Dylan Dias <dylandias@live.com>
2025-12-02 21:45:25 -0300

    Refactoring the class exercise pt2
    This changes should have been commited before

--- OneBitCode/E-22-Banco/Minha tentativa/entities/Loan.js
+++ OneBitCode/E-22-Banco/Minha tentativa/entities/Loan.js
@@ -1,25 +1,49 @@
+const Installment = require("./Installment");
+
 class Loan {
-  #value;
-  static #interestRate = 1.99;
-  #installment
-  #date
-  constructor(value, installment) {
-    this.#value = value;
-    this.#installment = installment;
-    this.#date = new Date().toLocaleString;
-  }
+    static #interestRate = 1.9;
+    #loan;
+    #date;
+    #totalInstallments;
+    #installments = [];
+    constructor(loanValue, totalInstallments){
+        this.#loan = loanValue;
+        this.#date = new Date().toLocaleDateString();
+        this.#totalInstallments = totalInstallments
+        for(let i = 1; i <= totalInstallments; i++){
+            let getValue = this.instValueCalculation;
+            this.#installments.push({Value: getValue, installment: i});
+        }
 
-    get showInsterestRate(){
-    return console.log(Loan.#interestRate);
-  }
+    }
 
-    set modifyInsterestRate(newValue){
-    let floatValue = parseFloat(newValue);
-    Loan.#interestRate = floatValue;
-  }
+    get instValueCalculation(){
+        let installmentValue = Math.round((this.#loan / this.#totalInstallments) * Loan.#interestRate);
+        return installmentValue;
+    }
+
+    static get getRate(){
+        return Loan.#interestRate;
+    }
+    static get checkInterestRate(){
+        console.log(`The current interest rate for a loan is: ${Loan.#interestRate}`)
+    }
+
+    get getLoan(){
+        console.log({
+            'Loan': this.#loan,
+            'Interest Rate:': Loan.#interestRate,
+            'Number of installments:': this.#totalInstallments,
+            'Date': this.#date,
+            'Installements': this.#installments,
+            'Total with interest:': this.#installments[0].Value * this.#totalInstallments
+        })
+    }
+
+    static set changeInterestRate(newValue){
+        Loan.#interestRate = newValue;
+    }
 }
 
-const salame = new Loan(500, 5);
-salame.showInsterestRate
-salame.modifyInsterestRate = 2.6;
-salame.showInsterestRate
+const salame = new Loan(100, 2);
+salame.getLoan;
